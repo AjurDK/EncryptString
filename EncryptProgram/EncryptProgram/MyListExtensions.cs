a14cc4b88d2b696aec4725dfd943dfb801ba8cc0
TECKSTORM\dk <dkajur.ee@gmail.com>
2018-06-02 01:30:42 +0530

    Encrypt string - Console application (POC) - initial check-in

---
+++ EncryptProgram/EncryptProgram/MyListExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EncryptProgram
+{
+    public static class MyListExtensions
+    {
+        public static IEnumerable<T> GetRange<T>(this List<T> list, int lastIndex, int startIndex = 0)
+        {
+            for (int i = startIndex; i < list.Count; i += lastIndex)
+            {
+                yield return list[i];
+            }
+        }
+
+        public static IEnumerable<T> GetRangeLinq<T>(this List<T> list, int lastIndex, int startIndex = 0)
+        {
+            return from i in Enumerable.Range(startIndex, ((list.Count - 1) / lastIndex) + 1)
+                   select list[lastIndex * i];
+        }
+
+        public static string GetEncryptedChar(this List<string> list, char charc, int key)
+        {
+            var str = !Char.IsNumber(charc) && Char.IsUpper(charc) ? charc.ToString().ToLower() : charc.ToString();
+            if (list.Contains(str))
+            {
+                var encryptedStr = list.ElementAtOrDefault(list.IndexOf(str) + key);
+                if (encryptedStr == null)
+                {
+                    encryptedStr = list.ElementAtOrDefault((list.IndexOf(str) + key) - list.Count);
+                }
+                if (!Char.IsNumber(charc) && Char.IsUpper(charc))
+                {
+                    return encryptedStr.ToUpper();
+                }
+                return encryptedStr;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ EncryptProgram/EncryptProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncryptProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = "Ab1-wx2";
+            var key = 4;
+
+            var numbers = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            var alphabets = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            var result = string.Empty;
+
+            foreach (char charc in word)
+            {
+                if (Char.IsLetterOrDigit(charc))
+                {
+                    result += alphabets.GetEncryptedChar(charc, key);
+                    result += numbers.GetEncryptedChar(charc, key);
+                }
+                else
+                {
+                    result += charc.ToString();
+                }
+            }
+
+            Console.WriteLine(word);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
